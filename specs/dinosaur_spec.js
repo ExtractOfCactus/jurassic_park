f93e45b78c8616b475c32bf772538c31c80debc1
Glen Young <extractofcactus@icloud.com>
2017-07-25 16:16:58 +0100

    dinosaur test file set up

---
+++ specs/dinosaur_spec.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+var Dinosaur = require("../disnosaur")
+
+describe("Dinosaur", function() {
+  var dino1;
+  var dino2;
+
+  beforeEach(function() {
+    dino1 = new Dinosaur("Tyrannosaurus", 1);
+    dino2 = new Dinosaur("Velociraptor", 3);
+  })
+
+}) 
