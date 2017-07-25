8e693ab68c8cba27089509add308d40be1bf20b8
Glen Young <extractofcactus@icloud.com>
2017-07-25 16:31:57 +0100

    park test file set up

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,21 +1,15 @@
 var assert = require("assert");
 var Dinosaur = require("../dinosaur");
+var Park = require("../park");
 
-describe("Dinosaur", function() {
+describe("Park", function(){
   var dino1;
   var dino2;
+  var park;
 
   beforeEach(function() {
     dino1 = new Dinosaur("Tyrannosaurus", 1);
     dino2 = new Dinosaur("Velociraptor", 3);
+    park = new Park();
   })
-
-  it("has type", function() {
-    assert.strictEqual(dino1.type, "Tyrannosaurus");
-  })
-
-  it("has offspring number", function() {
-    assert.strictEqual(dino2.offspring, 3);
-  })
-  
-}) 
+})
