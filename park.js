fb271b9fad618cc71cac7aa9e0b559f87f57929b
Glen Young <extractofcactus@icloud.com>
2017-07-25 17:01:41 +0100

    can remove type test passed

--- park.js
+++ park.js
@@ -10,4 +10,13 @@ Park.prototype.add = function(dinosaur) {
   this.enclosure.push(dinosaur);
 }
 
+Park.prototype.removeType = function(type) {
+  for (dino of this.enclosure) {
+    if (dino.type === type) {
+      var index = this.enclosure.indexOf(dino);
+      this.enclosure.splice(index, 1);
+    }
+  }
+}
+
 module.exports = Park;
